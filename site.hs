9fdcdd338078fb9191ff9f1a24cfec35d98721e4
Michael Tsang <michaeltsang1998@gmail.com>
2019-09-26 12:36:55 +0100

    Use nicer paths, white bg for info

--- site.hs
+++ site.hs
@@ -1,7 +1,10 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
 import           Data.Monoid (mappend)
+import           Data.List              (isInfixOf)
 import           Hakyll
+import           System.FilePath.Posix  (takeBaseName,takeDirectory
+                                         ,(</>),splitFileName)
 
 
 --------------------------------------------------------------------------------
@@ -16,11 +19,11 @@ main = hakyll $ do
         compile compressCssCompiler
 
     match (fromList ["about.md", "education.md", "experience.md", "other.md"]) $ do
-        route   $ setExtension "html"
+        route   $ niceRoute
         compile $ pandocCompiler
             >>= loadAndApplyTemplate "templates/information.html" defaultContext
             >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= relativizeUrls
+            >>= removeIndexHtml
 
     match "index.md" $ do
         route $ setExtension "html"
@@ -37,3 +40,25 @@ postCtx :: Context String
 postCtx =
     dateField "date" "%B %e, %Y" `mappend`
     defaultContext
+
+-- replace a foo/bar.md by foo/bar/index.html
+-- this way the url looks like: foo/bar in most browsers
+-- from http://yannesposito.com/Scratch/en/blog/Hakyll-setup/
+niceRoute :: Routes
+niceRoute = 
+    customRoute createIndexRoute
+    where
+        createIndexRoute ident = takeDirectory p </> takeBaseName p </> "index.html"
+            where p = toFilePath ident
+
+-- replace url of the form foo/bar/index.html by foo/bar
+-- from http://yannesposito.com/Scratch/en/blog/Hakyll-setup/
+removeIndexHtml :: Item String -> Compiler (Item String)
+removeIndexHtml item = 
+    return $ fmap (withUrls removeIndexStr) item
+    where
+        removeIndexStr :: String -> String
+        removeIndexStr url = case splitFileName url of
+            (dir, "index.html") | isLocal dir -> dir
+            _                                 -> url
+            where isLocal uri = not (isInfixOf "://" uri)
